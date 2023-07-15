76dc7ef7fd236174a94a79c629fb685601517edc
Amirxon <shokirovamir2006@gmail.com>
2023-07-16 02:51:31 +0500

    .

--- src/components/profitcard/chart/Chart.jsx
+++ src/components/profitcard/chart/Chart.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement } from "chart.js";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+} from "chart.js";
 
 ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
 
@@ -9,11 +15,17 @@ function Chart() {
     labels: ["May 12", "May 13", "May 14", "May 15", "May 16", "May 17"],
     datasets: [
       {
-        data: [8.2, 7.8, 9.5, 7.3, 6, 8.5, 8.2, 99, 35.5, 50, 22.5, 10.6],
-        backgroundColor: "transparent",
+        data: [8.2, 7.8, 2.5, 7.3, 6, 8.5, 8.2, 5.5, 0.5, 2, 22.5, 10.6],
+        backgroundColor: "#000",
         borderColor: "#000",
-        pointBorderColor: "transparent",
+        pointBorderColor: "#3b3b3b",
         pointBorderWidth: 4,
+        shadowColor: [
+          "rgba(255, 0, 0, 0.7)",
+          "rgba(0, 255, 0, 0.7)",
+          "rgba(0, 0, 255, 0.7)",
+        ],
+        shadowBlur: 20,
         tension: 0.5,
       },
     ],
@@ -33,6 +45,9 @@ function Chart() {
         ticks: {
           stepSize: 2,
           callback: (value) => value + "K",
+        },
+        font: {
+          family: "roboto-reg",
         },
       },
     },
