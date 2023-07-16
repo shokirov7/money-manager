5f14b7dc10ab9d216bd0ba7a024739ac69353934
Amirxon <shokirovamir2006@gmail.com>
2023-07-16 17:31:56 +0500

    gr

--- src/pages/workers/Workers.jsx
+++ src/pages/workers/Workers.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from '../../components/button/Button'
 import Profitcard from '../../components/profitcard/Profitcard'
 import './Workers.css'
+import WorkersTable from './WorkersTable'
 
 function Workers() {
   return (
@@ -11,61 +12,7 @@ function Workers() {
         <Profitcard desc={"Mahsulotlar"} />
         <Profitcard desc={"Umumiy daromad"} />
       </div>
-      <div className="workers_table">
-        <div className="workers_table_titles">
-          <div className="workers_table_title">id</div>
-          <div className="workers_table_title">Ism</div>
-          <div className="workers_table_title">Familiya</div>
-          <div className="workers_table_title">Username</div>
-          <div className="workers_table_title">Password</div>
-          <div className="workers_table_title">Ombor</div>
-          <div className="workers_table_title">KPI</div>
-          <div className="workers_table_title">Tel</div>
-          <Button text={"Qo'shish"}/>
-        </div>
-        <div className="workers_table_cards">
-          <div className="workers_table_card">
-            <div className="workers_table_title">1</div>
-            <div className="workers_table_title">Amir</div>
-            <div className="workers_table_title">Gerbiona</div>
-            <div className="workers_table_title">granger22</div>
-            <div className="workers_table_title">shokiriv2006</div>
-            <div className="workers_table_title">1</div>
-            <div className="workers_table_title">22</div>
-            <div className="workers_table_title">+998911591516</div>
-          </div>
-          <div className="workers_table_card">
-            <div className="workers_table_title">1</div>
-            <div className="workers_table_title">Amir</div>
-            <div className="workers_table_title">Gerbiona</div>
-            <div className="workers_table_title">granger22</div>
-            <div className="workers_table_title">shokiriv2006</div>
-            <div className="workers_table_title">1</div>
-            <div className="workers_table_title">22</div>
-            <div className="workers_table_title">+998911591516</div>
-          </div>
-          <div className="workers_table_card">
-            <div className="workers_table_title">1</div>
-            <div className="workers_table_title">Amir</div>
-            <div className="workers_table_title">Gerbiona</div>
-            <div className="workers_table_title">granger22</div>
-            <div className="workers_table_title">shokiriv2006</div>
-            <div className="workers_table_title">1</div>
-            <div className="workers_table_title">22</div>
-            <div className="workers_table_title">+998911591516</div>
-          </div>
-          <div className="workers_table_card">
-            <div className="workers_table_title">1</div>
-            <div className="workers_table_title">Amir</div>
-            <div className="workers_table_title">Gerbiona</div>
-            <div className="workers_table_title">granger22</div>
-            <div className="workers_table_title">shokiriv2006</div>
-            <div className="workers_table_title">1</div>
-            <div className="workers_table_title">22</div>
-            <div className="workers_table_title">+998911591516</div>
-          </div>
-        </div>
-      </div>
+      <WorkersTable role={role}/>
     </div>
   )
 }
