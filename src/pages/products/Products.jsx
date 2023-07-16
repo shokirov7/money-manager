f544d8ef44b001fb827716987adb36735f3e58f4
nolonooojx23 <119511297+nolonooojx23@users.noreply.github.com>
2023-07-16 03:42:25 +0300

    something2

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import Profitcard from '../../components/profitcard/Profitcard'
 import './Products.css'
 
 function Products() {
   return (
-    <div>
-      <div className="products_welcome">Products!</div>
+    <div className='products'>
+      <div className="products_welcome">Mahsulotlar!</div>
+      <div className="products_top">
+        <Profitcard desc={"Mahsulotlar"} />
+        <Profitcard desc={"Umumiy daromad"} />
+      </div>
+      <div className="products_table">
+        <div className="products_table_titles">
+          <div className="products_table_title">id</div>
+          <div className="products_table_title">Mahsulot nomi</div>
+          <div className="products_table_title">Tannarx</div>
+          <div className="products_table_title">Sotuv Narx</div>
+          <div className="products_table_title">Mahsulot brendi</div>
+        </div>
+        <div className="products_table_cards">
+          <div className="products_table_card">
+            <div className="products_table_title">1</div>
+            <div className="products_table_title">Fanta</div>
+            <div className="products_table_title">10000$</div>
+            <div className="products_table_title">12000$</div>
+            <div className="products_table_title">Coca Cola</div>
+          </div>
+          <div className="products_table_card">
+            <div className="products_table_title">1</div>
+            <div className="products_table_title">Fanta</div>
+            <div className="products_table_title">10000$</div>
+            <div className="products_table_title">12000$</div>
+            <div className="products_table_title">Coca Cola</div>
+          </div>
+          <div className="products_table_card">
+            <div className="products_table_title">1</div>
+            <div className="products_table_title">Fanta</div>
+            <div className="products_table_title">10000$</div>
+            <div className="products_table_title">12000$</div>
+            <div className="products_table_title">Coca Cola</div>
+          </div>
+          <div className="products_table_card">
+            <div className="products_table_title">1</div>
+            <div className="products_table_title">Fanta</div>
+            <div className="products_table_title">10000$</div>
+            <div className="products_table_title">12000$</div>
+            <div className="products_table_title">Coca Cola</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
