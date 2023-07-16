74af37f914b879a6de6d8b4ef39434190303b1e4
azamat-developer <azamatjonazimov561@gmail.com>
2023-07-16 17:44:29 +0500

    I added reducer

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,28 @@ import Filial from "./pages/filial/Filial";
 import Stats from "./pages/stats/Stats";
 import Archive from "./pages/archive/Archive";
 import Sidebar from "./components/sidebar/Sidebar";
+import Login from './pages/login/Login'
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Sidebar />
-        <div className="app_right">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/workers" element={<Workers />} />
-            <Route path="/filials" element={<Filial />} />
-            <Route path="/statistic" element={<Stats />} />
-            <Route path="/archive" element={<Archive />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+		<div className='App'>
+			<BrowserRouter>
+				<Sidebar />
+				<div className='app_right'>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/products' element={<Products />} />
+						<Route path='/workers' element={<Workers />} />
+						<Route path='/filials' element={<Filial />} />
+						<Route path='/statistic' element={<Stats />} />
+						<Route path='/archive' element={<Archive />} />
+						<Route path='/login' element={<Login />} />
+						<Route path='*' element={<Navigate to='/' replace />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</div>
+	)
 }
 
 export default App;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,36 +1,76 @@
 import React, { useState } from "react";
 import './Login.css'
+import authService from '../../api/axios'
+import { useDispatch } from 'react-redux'
+import { signUserStart } from '../../reducers/auth'
 
 function Login() {
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const dispatch = useDispatch()
 
-    const handleLoginEvent = (e) => {
-        e.preventDefault()
-        let userCredential={
-            username, password
-        }
-
-    }
+    const handleSubmit = async e => {
+			e.preventDefault()
+			const user = {
+				username: username,
+				password: password,
+			}
+			dispatch(signUserStart)
+			try {
+				// if (isAdmin === 'admin') {
+					if (user.username === 'admin' && user.password === '123') {
+            const data = await authService.userLogin(user)
+						// const { data } = await authService.userLogin(user)
+						// dispatch(signUserSuccess(data))
+					} else {
+						alert("Siz admin nomidan kira olishingiz uchun sizda ruxsat yo'q !")
+					}
+				// } else if (isAdmin === 'user') {
+					// if (user.username !== 'admin' && user.password !== '123') {
+					// 	// const { data } = await authService.userLogin(user)
+					// 	// dispatch(signUserSuccess(data))
+					// 	console.log(data)
+					// } else {
+					// 	alert("Siz rolni noto'g'ri tanladingiz !")
+					// }
+					// dispatch(getUserDetails(data))
+				// }
+				// return
+			} catch (error) {
+				// alert(error.request.response)
+			}
+		}
 
   return (
-    <div className="login">
-      <form class="form" onSubmit={handleLoginEvent}>
-        <p class="form-title">Sign in</p>
-        <div class="input-container">
-          <input type="email" placeholder="Username" required value={email} onChange={(e) =>setUsername(e.target.value)}/>
-          <span></span>
-        </div>
-        <div class="input-container">
-          <input type="password" placeholder="Password" required value={password} onChange={(e) =>setPassword(e.target.value)}/>
-        </div>
-        <button type="submit" class="submit">
-          Sign in
-        </button>
-      </form>
-    </div>
-  );
+		<div className='login'>
+			<form class='form' onSubmit={handleSubmit}>
+				<p class='form-title'>Sign in</p>
+				<div class='input-container'>
+					<input
+						type='text'
+						placeholder='Username'
+						required
+						value={username}
+						onChange={e => setUsername(e.target.value)}
+					/>
+					<span></span>
+				</div>
+				<div class='input-container'>
+					<input
+						type='password'
+						placeholder='Password'
+						required
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+					/>
+				</div>
+				<button type='submit' class='submit'>
+					Sign in
+				</button>
+			</form>
+		</div>
+	)
 }
 
 export default Login;
