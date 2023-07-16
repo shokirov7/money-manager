f942611279dc71ec6dedfbf1213b9065414aee5c
nolonooojx23 <119511297+nolonooojx23@users.noreply.github.com>
2023-07-16 04:33:30 +0300

    Filials add

--- src/pages/filial/Filial.jsx
+++ src/pages/filial/Filial.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import Button from '../../components/button/Button'
+import Profitcard from '../../components/profitcard/Profitcard'
+import './Filial.css'
 
 function Filial() {
   return (
-    <div>
-      
+    <div className='filials'>
+      <div className="filials_welcome">Mahsulotlar</div>
+      <div className="filials_top">
+        <Profitcard desc={"Mahsulotlar"} />
+        <Profitcard desc={"Umumiy daromad"} />
+      </div>
+      <div className="filials_table">
+        <div className="filials_table_titles">
+          <div className="filials_table_title">id</div>
+          <div className="filials_table_title">Manzil</div>
+          <div className="filials_table_title">Telefon raqami</div>
+          <div className="filials_table_title">Nom</div>
+          <Button />
+        </div>
+        <div className="filials_table_cards">
+          <div className="filials_table_card">
+            <div className="filials_table_title">1</div>
+            <div className="filials_table_title">Marg'ilon sh. Sohibqiron Temur k. 1-uy</div>
+            <div className="filials_table_title">+998907791449</div>
+            <div className="filials_table_title">Namangan</div>
+          </div>
+          <div className="filials_table_card">
+            <div className="filials_table_title">1</div>
+            <div className="filials_table_title">Marg'ilon sh. Sohibqiron Temur k. 1-uy</div>
+            <div className="filials_table_title">+998907791449</div>
+            <div className="filials_table_title">Namangan</div>
+          </div>
+          <div className="filials_table_card">
+            <div className="filials_table_title">1</div>
+            <div className="filials_table_title">Marg'ilon sh. Sohibqiron Temur k. 1-uy</div>
+            <div className="filials_table_title">+998907791449</div>
+            <div className="filials_table_title">Namangan</div>
+          </div>
+          <div className="filials_table_card">
+            <div className="filials_table_title">1</div>
+            <div className="filials_table_title">Marg'ilon sh. Sohibqiron Temur k. 1-uy</div>
+            <div className="filials_table_title">+998907791449</div>
+            <div className="filials_table_title">Namangan</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
