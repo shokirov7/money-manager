8c846087d5f623d4ad118c256b9c662ca5d8fe6e
azamat-developer <azamatjonazimov561@gmail.com>
2023-07-16 18:34:17 +0500

    ending Login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,6 +3,7 @@ import './Login.css'
 import authService from '../../api/axios'
 import { useDispatch } from 'react-redux'
 import { signUserFailure, signUserStart, signUserSuccess } from '../../reducers/auth'
+import { getItem, setItem } from '../../helpers/persistence-log'
 
 function Login() {
 	const [isAdmin, setAdmin] = useState(
@@ -19,28 +20,26 @@ function Login() {
 			}
 			dispatch(signUserStart)
 			try {
-				// if (isAdmin === 'admin') {
-					// if (user.username === 'admin' && user.password === '123') {
+				if (isAdmin === 'admin') {
+					if (user.username === 'admin' && user.password === '123') {
             const {data} = await authService.userLogin(user)
 						dispatch(signUserSuccess(data))
-					// } else {
-					// 	alert("Siz admin nomidan kira olishingiz uchun sizda ruxsat yo'q !")
-					// }
-				// } else if (isAdmin === 'user') {
-					// if (user.username !== 'admin' && user.password !== '123') {
-					// 	// const { data } = await authService.userLogin(user)
-					// 	// dispatch(signUserSuccess(data))
-					// 	console.log(data)
-					// } else {
-					// 	alert("Siz rolni noto'g'ri tanladingiz !")
-					// }
-					// dispatch(getUserDetails(data))
-				// }
-				// return
+					} else {
+						alert("Siz admin nomidan kira olishingiz uchun sizda ruxsat yo'q !")
+					}
+				} else if (isAdmin === 'user') {
+					if (user.username !== 'admin' && user.password !== '123') {
+						const { data } = await authService.userLogin(user)
+						dispatch(signUserSuccess(data))
+						console.log(data)
+					} else {
+						alert("Siz rolni noto'g'ri tanladingiz !")
+					}
+					dispatch(getUserDetails(data))
+				}
+				return
 			} catch (error) {
-        // dispatch(signUserFailure(error))
 				console.log(error);
-				// alert(error.request.response)
 			}
 		}
 
@@ -48,17 +47,19 @@ function Login() {
 		<div className='login'>
 			<form class='form' onSubmit={handleSubmit}>
 				<p class='form-title'>Sign in</p>
-				<select
-					className='outline-none rounded-md bg-transparent w-full m-1 border border-[#334155]'
-					onChange={e => {
-						setAdmin(e.target.value)
-						setItem('role', e.target.value)
-					}}
-					value={isAdmin}
-				>
-					<option value='admin'>Admin</option>
-					<option value='user'>Xodim</option>
-				</select>
+				<div className='input-container'>
+					<select
+						className='outline-none rounded-md bg-transparent w-full m-1 border border-[#334155]'
+						onChange={e => {
+							setAdmin(e.target.value)
+							setItem('role', e.target.value)
+						}}
+						value={isAdmin}
+					>
+						<option value='admin'>Admin</option>
+						<option value='user'>Xodim</option>
+					</select>
+				</div>
 				<div class='input-container'>
 					<input
 						type='text'
